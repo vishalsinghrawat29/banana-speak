912172cc32986262b091f9c8e8bf0b019365902c
Vishal Singh Rawat <vsrawat2906@gmail.com>
2021-01-14 22:01:06 +0530

    update index.html

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-console.log("vishal");
+// console.log("vishal");
 
 
-//input
-var username = prompt("give me username");
+// //input
+// var username = prompt("give me username");
 
-//process
-var welcomemessage = "welcome " + username;
+// //process
+// var welcomemessage = "welcome " + username;
 
-//output
-alert(welcomemessage);
+// //output
+// alert(welcomemessage);
+
+var btnTranslate = document.querySelector("#btn-translate");
+btnTranslate.addEventListener("click", function clickEventHandler () {
+    console.log("click");
+})
