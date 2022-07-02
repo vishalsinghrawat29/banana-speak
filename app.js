00d6f20c6281c5f35d7c8546e39dc7cfba5b62b3
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2022-07-02 07:50:51 +0530

    reference outputDiv in JS

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // input
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-
+var outputDiv =  document.querySelector("#output");
+outputDiv.innerText = "Vishal Singh";
+console.log(outputDiv);
 function clickHandler() {
         console.log("clicked");
         console.log("input:", txtInput.value);
