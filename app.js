e5bc12c0a232cef9ac096071354b4c42647c7f35
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2022-07-03 09:01:41 +0530

    error handling

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 function getTransalationURL(text){
        return serverUrl+"?" + "Text=" + text
 }
+function errorHandler(error){
+       console.log("error occured"+error);
+       alert("something wrong with server! Try again after sometime.");
+}
 function clickHandler() {
        var inputText =  txtInput.value ; //taking input
 
        //calling server for processing
        fetch(getTransalationURL(inputText))
        .then(response => response.json())
-       .then(json => console.log(json))
+       .then(json => console.log(json.contents.translated))
+       .catch(errorHandler)
 };
 
 
