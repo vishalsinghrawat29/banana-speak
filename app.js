b6aa0cb9b11f1ed750910abcc15105c9e2df6828
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2022-07-03 09:15:57 +0530

    replace the mock url

--- app.js
+++ app.js
@@ -2,21 +2,27 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv =  document.querySelector("#output");
-var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
 function getTransalationURL(text){
-       return serverUrl+"?" + "Text=" + text
+       return serverUrl+"?" + "text=" + text
 }
+
 function errorHandler(error){
-       console.log("error occured"+error);
+       console.log("error occured"+ error);
        alert("something wrong with server! Try again after sometime.");
 }
+
 function clickHandler() {
        var inputText =  txtInput.value ; //taking input
 
        //calling server for processing
        fetch(getTransalationURL(inputText))
        .then(response => response.json())
-       .then(json => console.log(json.contents.translated))
+       .then(json => {
+              var translatedText = json.contents.translated;
+              outputDiv.innerText = translatedText;
+              })
        .catch(errorHandler)
 };
 
