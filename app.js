0b3459f8ec2306e176b8d3af1bae9b8cd965e902
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2022-07-03 08:51:04 +0530

    on click btn, fetch call fire

--- app.js
+++ app.js
@@ -2,10 +2,18 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv =  document.querySelector("#output");
-
-function clickHandler() {
-       outputDiv.innerText = "askfhka" + txtInput.value;
+var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+function getTransalationURL(text){
+       return serverUrl+"?" + "Text=" + text
 }
+function clickHandler() {
+       var inputText =  txtInput.value ; //taking input
+
+       //calling server for processing
+       fetch(getTransalationURL(inputText))
+       .then(response => response.json())
+       .then(json => console.log(json))
+};
 
 
 btnTranslate.addEventListener("click", clickHandler)
